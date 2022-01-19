6ddd295d62f363157e6cb0e3159d354eddaabd77
ReemAlshathly <reem250139@gmail.com>
2022-01-19 03:17:30 +0300

    update.new

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const showBtnim=document.getElementsByClassName("show-btn-im");
+const model_img=document.getElementById('model-img');
+
+for (let i=0 ; i< showBtn.length ; i++){
+showBtnim[i].addEventListener('click', function(){
+var img = showBtnim[i].getAttribute('src');
+model_img.setAttribute('src',img);
+
+
+})
+}
